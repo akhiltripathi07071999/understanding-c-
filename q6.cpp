836305ca2461c396f083a9867cd499f48d6a5862
akhiltripathi07071999 <42205097+akhiltripathi07071999@users.noreply.github.com>
2018-10-17 18:51:05 +0530

    Add files via upload

---
+++ q6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void printreverse(int n){
+     if(n<10) cout <<n;
+     else{
+         cout <<n%10;
+         printreverse(n/10);
+          }
+}
+int main(){
+int n;
+cout<<"Enter the numbers which you want to be reversed"<<endl;
+cin>>n;
+printreverse(n);
+return 0;
+}
